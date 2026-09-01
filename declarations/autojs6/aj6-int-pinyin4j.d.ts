29580cef02c72caebde0e91167c2f123acf7f140
SuperMonster003 <wu2du2mo2gu0@gmail.com>
2026-09-01 19:19:46 +0800

    feat(pinyin4j): declare readings API

--- declarations/autojs6/aj6-int-pinyin4j.d.ts
+++ declarations/autojs6/aj6-int-pinyin4j.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: SuperMonster003 <https://github.com/SuperMonster003>
 // TypeScript Version: 4.8.4
 //
-// Last modified: Jul 27, 2026
+// Last modified: Sep 1, 2026
 
 /// <reference path="./index.d.ts" />
 
@@ -19,11 +19,25 @@ declare namespace Internal {
         of(source: string, options?: string | Pinyin4j.Options): string;
 
         "as"(source: string): string;
+
+        readings(source: string, options?: Pinyin4j.ReadingsOptions): Pinyin4j.Matrix;
     }
 
 }
 
 declare namespace Pinyin4j {
+
+    type RomanizationSystem = 'HANYU' | 'HANYU_PINYIN' | 'PINYIN'
+        | 'TONGYONG' | 'TONGYONG_PINYIN'
+        | 'WADE' | 'WADE_GILES' | 'WADEGILES' | 'WADE_GILES_PINYIN'
+        | 'MPS2' | 'MANDARIN_PHONETIC_SYMBOLS_2'
+        | 'YALE' | 'YALE_PINYIN'
+        | 'GWOYEU' | 'GWOYEU_ROMATZYH' | 'GR'
+        | string;
+
+    type Row = string[];
+
+    type Matrix = Row[];
 
     type CaseType = 'LOWERCASE' | 'LOW' | 'L' | '0' | 'UPPERCASE' | 'UP' | 'U' | '1'
         | string;
@@ -47,6 +61,25 @@ declare namespace Pinyin4j {
         toneType?: ToneType;
         v?: VCharType;
         vChar?: VCharType;
+        vCharType?: VCharType;
+    }
+
+    interface ReadingsOptions {
+        romanization?: RomanizationSystem;
+        system?: RomanizationSystem;
+        /** Hanyu Pinyin only. */
+        case?: CaseType;
+        /** Hanyu Pinyin only. */
+        caseType?: CaseType;
+        /** Hanyu Pinyin only. Defaults to WITH_TONE_NUMBER. */
+        tone?: ToneType;
+        /** Hanyu Pinyin only. Defaults to WITH_TONE_NUMBER. */
+        toneType?: ToneType;
+        /** Hanyu Pinyin only. */
+        v?: VCharType;
+        /** Hanyu Pinyin only. */
+        vChar?: VCharType;
+        /** Hanyu Pinyin only. */
         vCharType?: VCharType;
     }
 
